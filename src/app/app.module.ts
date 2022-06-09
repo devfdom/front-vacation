beffdac1ca1132936dd21a9fe4922ac9a4ad37ba
Lupe13 <lupe.flores.r@gmail.com>
2022-06-09 10:13:25 +0200

    logo/navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
