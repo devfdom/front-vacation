c0814dc8ecf80c8dfee240bd63d026311a4eb533
PMuin <99225997+PMuin@users.noreply.github.com>
2022-06-09 11:22:56 +0200

    created components owner&user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OwnerComponent } from './owner/owner.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OwnerComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
