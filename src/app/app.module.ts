8bfe55d14910aafe150712fbe69a90668ef272bc
devfdom@gmail.com <devfdom@gmail.com>
2022-06-09 20:56:41 +0200

    fixed merge errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
-import { NgModule, Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AlladvertiseComponent } from './alladvertise/alladvertise.component';
+import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './header/header.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,11 +15,22 @@ import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent,
+    ContactComponent,
+    OwnerComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule.forRoot([
+      {path:'properties', component:AlladvertiseComponent},
+      {path:'contact', component:ContactComponent},
+      {path:'owner', component:OwnerComponent},
+      {path:'user', component:UserComponent}
+    ])
+
 
   ],
   providers: [],
