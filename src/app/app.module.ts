d542ca824d252d14019333e901eb53692e46e4e1
PMuin <99225997+PMuin@users.noreply.github.com>
2022-06-13 10:28:59 +0200

    cache angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
+
 import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OwnerComponent } from './owner/owner.component';
-import { UserComponent } from './user/user.component';
+import { RouterModule } from '@angular/router';
 
+@NgModule({
+  declarations: [
+    AppComponent,
+    OwnerComponent
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule,
-
+    RouterModule.forRoot([
+    {path:'owner', component: OwnerComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
