6e9abb19f91a03f7bdf02250099dfc6aa00a401e
devfdom@gmail.com <devfdom@gmail.com>
2022-06-13 11:44:00 +0200

    add background-color

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,23 @@
 import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
 
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactComponent
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule.forRoot([
+      {path:'contact', component: ContactComponent}
+      ])
 
   ],
   providers: [],
