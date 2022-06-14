45372c45a9882207a4b5cf85d733edfa4f2f0fcc
PMuin <99225997+PMuin@users.noreply.github.com>
2022-06-14 10:06:54 +0200

    css update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,35 @@
-
 import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AlladvertiseComponent } from './alladvertise/alladvertise.component';
+import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './header/header.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OwnerComponent } from './owner/owner.component';
-import { RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OwnerComponent
+    FooterComponent,
+    HeaderComponent,
+    ContactComponent,
+    OwnerComponent,
+    UserComponent,
+    HomeComponent
   ],
 
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot([
-    {path:'owner', component: OwnerComponent}
+      {path:'properties', component:AlladvertiseComponent},
+      {path:'contact', component:ContactComponent},
+      {path:'owner', component:OwnerComponent},
+      {path: '', component: HomeComponent},
+      {path:'user', component:UserComponent}
     ])
   ],
   providers: [],
@@ -25,3 +37,4 @@ import { RouterModule } from '@angular/router';
 })
 
 export class AppModule { }
+
