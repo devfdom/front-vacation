4874d5fa30e0e643533b57057e2cf732f4391646
devfdom@gmail.com <devfdom@gmail.com>
2022-06-14 13:34:21 +0200

    add html content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { RouterModule } from '@angular/router';
-import { AlladvertiseComponent } from './alladvertise/alladvertise.component';
 import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -13,11 +13,8 @@ import { HeaderComponent } from './header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    RouterModule.forRoot([{
-      path:'properties', component:AlladvertiseComponent
-    }])
-    
+    AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
