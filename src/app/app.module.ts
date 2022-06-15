54238413aacaa26e717fef0916e1c0f1e76b4a2d
devfdom@gmail.com <devfdom@gmail.com>
2022-06-15 15:08:39 +0200

    adapting the design to the browser

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,38 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { ContactComponent } from './contact/contact.component';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
+import { ContactComponent } from "./contact/contact.component";
+import { HomeComponent } from "./home/home.component";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { OwnerComponent } from "./owner/owner.component";
+import { UserComponent } from "./user/user.component";
+
+import { FooterComponent } from "./footer/footer.component";
 import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { OwnerComponent } from './owner/owner.component';
-import { UserComponent } from './user/user.component';
-
-import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    HeaderComponent,
     ContactComponent,
     OwnerComponent,
     UserComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HeaderComponent,
     RouterModule.forRoot([
-      {path:'contact', component:ContactComponent},
-      {path:'owner', component:OwnerComponent},
-      {path: '', component: HomeComponent},
-      {path:'user', component:UserComponent}
-    ])
-
+      { path: "contact", component: ContactComponent },
+      { path: "owner", component: OwnerComponent },
+      { path: "", component: HomeComponent },
+      { path: "user", component: UserComponent },
+    ]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
