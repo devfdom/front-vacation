e05b6338ccad29f75af18e0a237b87a29a52dc9c
ElisabethIld <99247125+ElisabethIld@users.noreply.github.com>
2022-06-15 14:51:39 +0200

    eachadvertisement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,28 @@
-import { HomeComponent } from './home/home.component';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { AlladvertiseComponent } from './alladvertise/alladvertise.component';
-import { ContactComponent } from './contact/contact.component';
-import { HeaderComponent } from './header/header.component';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { OwnerComponent } from './owner/owner.component';
-import { UserComponent } from './user/user.component';
+import { EachadvertisementComponent } from './eachadvertisement/eachadvertisement.component';
 
-import { FooterComponent } from './footer/footer.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent,
-    HeaderComponent,
-    ContactComponent,
-    OwnerComponent,
-    UserComponent,
-    HomeComponent
+    EachadvertisementComponent,
+   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot([
-      {path:'properties', component:AlladvertiseComponent},
-      {path:'contact', component:ContactComponent},
-      {path:'owner', component:OwnerComponent},
-      {path: '', component: HomeComponent},
-      {path:'user', component:UserComponent}
+      {path:'infoproperty', component:EachadvertisementComponent},
+     
     ])
 
   ],
