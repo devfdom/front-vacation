abb52c9a2ee149b52e66be9aefecb4f9d103d185
devfdom@gmail.com <devfdom@gmail.com>
2022-06-21 09:44:56 +0200

    add filter x City

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,17 @@
-import { PropertiesService } from './services/properties.service';
-import { Component, OnInit } from "@angular/core";
-import { constructor } from 'jasmine';
+import { Component } from "@angular/core";
+
 
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
-  providers: [PropertiesService]
+  // providers: [PropertiesService]
 })
-export class AppComponent implements OnInit {
-  title = 'properties';
-  public properties: Array<any> = []
-  filterText: string = '';
+export class AppComponent {
+  title = 'properties x city';
+  propertiesList: any = ["Property 1", "Property 2", "Property 3", "Property 4", "Property 5", "User 6", "Test", "TT"];
 
   }
 
-  constructor(private propertiesService: PropertiesService){
-
-  }
 
--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -1,19 +1,39 @@
-import { Properties } from './model/properties';
+// import { Properties } from './model/properties';
+// import { Pipe, PipeTransform } from '@angular/core';
+
+// @Pipe({
+//   name: 'filterCities'
+// })
+// export class FilterCitiesPipe implements PipeTransform {
+
+//   transform(properties: Properties[], filterText: String){
+//     if(properties.length === 0 || filterText === ''){
+//       return properties;
+//     } else {
+//       return properties.filter((properties) =>
+//       {
+//         return properties.City.toLowerCase() === filterText.toLowerCase()
+//       })
+//     }
+//   }
+// }
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'filterCities'
+    name: 'filter'
 })
-export class FilterCitiesPipe implements PipeTransform {
+export class FilterPipe implements PipeTransform {
 
-  transform(properties: Properties[], filterText: String){
-    if(properties.length === 0 || filterText === ''){
-      return properties;
-    } else {
-      return properties.filter((properties) =>
-      {
-        return properties.City.toLowerCase() === filterText.toLowerCase()
-      })
+    transform(items: any[], text: string): any[] {
+
+        if (!items) { return []; }
+
+        if (!text) { return items; }
+
+        text = text.toLowerCase();
+        return items.filter(x => {
+            return x.toLowerCase().includes(text);
+        });
     }
-  }
 }
+
