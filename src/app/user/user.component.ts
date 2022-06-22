f154a5bbc985dfdfed636a865c02c9fa715168b3
PMuin <99225997+PMuin@users.noreply.github.com>
2022-06-22 08:50:18 +0200

    css update

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
@@ -11,5 +12,4 @@ export class UserComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
 }
