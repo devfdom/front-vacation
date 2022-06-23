d5ed89ed05e35a45e40f02a74300c78ef9d73826
devfdom@gmail.com <devfdom@gmail.com>
2022-06-23 10:25:24 +0200

    token-storage.service.ts mod

--- src/app/services/properties.service.ts
+++ src/app/services/properties.service.ts
@@ -11,10 +11,6 @@ import { catchError, map, Observable, throwError } from "rxjs";
   providedIn: "root",
 })
 export class PropertiesService {
-  constructor(private http: HttpClient) {
-    console.log("Properties Services");
-  }
-
   private handleError(error: HttpErrorResponse): any {
     if (error.error instanceof ErrorEvent) {
       console.error("An error occurred:", error.error.message);
@@ -46,80 +42,50 @@ export class PropertiesService {
         headers:header});*/
     }
 
-    //this one and the next should do the same thing - Create a new property
-    //this one and the next should do the same thing - Create a new property
+//this one and the next should do the same thing - Find property by Id
+public idProperty(id: number): Observable<Properties> {
+  return this.http.get<Properties>(
+    `${this.apiServerUrl}/petvacation/properties/${id}`
+  );
+}
 
-  public registerProperty(...args: [properties: Properties]): Observable<Properties> {
-    return this.http.post<Properties>(
-      `${this.apiServerUrl}/petvacation/properties/create`,
+getProperty(id: number): Observable<any> {
+  return this.http
+    .get(`${this.apiServerUrl}/petvacation/properties/${id}`)
+    .pipe(catchError(this.handleError));
+}
+
+//Edit property
+updateProperty(id: number, properties: Properties): Observable<any> {
+  return this.http
+    .put<Properties>(
+      `${this.apiServerUrl}/petvacation/properties/${id}`,
       properties
-    );
-  public property: Properties | undefined;
+    )
+    .pipe(catchError(this.handleError));
+}
 
-  getProperties(): Observable<any> {
-    return this.http
-      .get(`${this.apiServerUrl}/petvacation/properties`)
-      .pipe(catchError(this.handleError));
-  }
-
-  getProperty(id: number): Observable<any> {
-    return this.http
-      .get<Properties>(`${this.apiServerUrl}/petvacation/properties/${id}`)
-      .pipe(catchError(this.handleError));
-  }
-
-  public addProperty(properties: any): Observable<any> {
-    console.log(properties);
-    return this.http
-      .post<Properties>(
-        `${this.apiServerUrl}/petvacation/properties/create`,
-        properties
-      )
-      .pipe(catchError(this.handleError));
-  }
-
-  updateProperty(id: number, properties: Properties): Observable<any> {
-    return this.http
-      .put<Properties>(
-        `${this.apiServerUrl}/petvacation/properties/${id}`,
-        properties
-      )
-      .pipe(catchError(this.handleError));
-  }
-
-  deleteProperty(id: number): Observable<any> {
-    return this.http
-      .delete<Properties>(
-        `${this.apiServerUrl}/petvacation/properties/delete/${id}`
-      )
-      .pipe(catchError(this.handleError));
-  }
+//Delete property
+deleteProperty(id: number): Observable<any> {
+  return this.http
+    .delete<Properties>(
+      `${this.apiServerUrl}/petvacation/properties/delete/${id}`
+    )
+    .pipe(catchError(this.handleError));
 }
 
 /*
-    public registerUser(user:User):Observable<User>{
-      return this.http.post<Properties>(`${this.apiServerUrl}/petvacation/user/save`, user);
+  public registerUser(user:User):Observable<User>{
+    return this.http.post<Properties
+
+    >(`${this.apiServerUrl}/petvacation/user/save`, user);
     }
 
     public removeUser(user:User):Observable<User>{
       return this.http.post<User>(`${this.apiServerUrl}/petvacation/user/delete/`, user);
     }*/
+}
 
 function properties<T>(arg0: string, properties: any): Observable<Properties> {
   throw new Error('Function not implemented.');
 }
-
-// //this one and the next should do the same thing - Create a new property
-// public registerProperty(properties: Properties): Observable<Properties> {
-//   return this.http.post<Properties>(
-//     `${this.apiServerUrl}/petvacation/properties/create`,
-//     properties
-//   );
-// }
-
-//this one and the next should do the same thing - Find property by Id
-// public idProperty(id: number): Observable<Properties> {
-//   return this.http.get<Properties>(
-//     `${this.apiServerUrl}/petvacation/properties/${id}`
-//   );
-// }
